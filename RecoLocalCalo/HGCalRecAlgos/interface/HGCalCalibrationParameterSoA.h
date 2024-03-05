b25c44678b0acea61e7dc6657f5b0393defab32a
Arnaud Steen <arnaud.steen@cern.ch>
2024-03-05 15:56:53 +0100

    add examplary values

--- RecoLocalCalo/HGCalRecAlgos/interface/HGCalCalibrationParameterSoA.h
+++ RecoLocalCalo/HGCalRecAlgos/interface/HGCalCalibrationParameterSoA.h
@@ -15,17 +15,17 @@ namespace hgcalrechit {
   // Generate structure of channel-level arrays (SoA) layout with RecHit dataformat
   GENERATE_SOA_LAYOUT(HGCalCalibParamSoALayout,
                       SOA_SCALAR(HGCalCalibrationParameterIndex, config), // map elecId <-> dense idx
-                      SOA_COLUMN(float, ADC_ped),     // ADC pedestals
-                      SOA_COLUMN(float, CM_slope),    // common mode slope
-                      SOA_COLUMN(float, CM_ped),      // common mode pedestal (offset)
-                      SOA_COLUMN(float, BXm1_slope),  // leakage correction from previous bunch
-                      SOA_COLUMN(float, ADCtofC),     // ADC conversion to charge (fC)
-                      SOA_COLUMN(float, TOTtofC),     // TOT conversion to charge (fC)
-                      SOA_COLUMN(float, TOT_ped),     // TOT pedestal (offset)
-                      SOA_COLUMN(float, TOT_lin),     // threshold at which TOT is linear
-                      SOA_COLUMN(float, TOT_P0),      // coefficient pol2 in nonlinear region
-                      SOA_COLUMN(float, TOT_P1),      // coefficient pol2 in nonlinear region
-                      SOA_COLUMN(float, TOT_P2),      // coefficient pol2 in nonlinear region
+                      SOA_COLUMN(float, ADC_ped),     // ADC pedestals, O(91)
+                      SOA_COLUMN(float, CM_slope),    // common mode slope, O(0.25)
+                      SOA_COLUMN(float, CM_ped),      // common mode pedestal (offset), O(92)
+                      SOA_COLUMN(float, BXm1_slope),  // leakage correction from previous bunch, O(0.0)
+                      SOA_COLUMN(float, ADCtofC),     // ADC conversion to charge (fC), depends on gain (80, 160, 320 fC)
+                      SOA_COLUMN(float, TOTtofC),     // TOT conversion to charge (fC), depends on gain (80, 160, 320 fC)
+                      SOA_COLUMN(float, TOT_ped),     // TOT pedestal (offset), O(9.0)
+                      SOA_COLUMN(float, TOT_lin),     // threshold at which TOT is linear, O(200)
+                      SOA_COLUMN(float, TOT_P0),      // coefficient pol2 in nonlinear region, O(145)
+                      SOA_COLUMN(float, TOT_P1),      // coefficient pol2 in nonlinear region, O(1.0)
+                      SOA_COLUMN(float, TOT_P2),      // coefficient pol2 in nonlinear region, O(0.004)
                       SOA_COLUMN(float, TOAtops),     // TOA conversion to time (ps)
                       SOA_COLUMN(bool,  valid)        // if false: mask dead channel
   )
