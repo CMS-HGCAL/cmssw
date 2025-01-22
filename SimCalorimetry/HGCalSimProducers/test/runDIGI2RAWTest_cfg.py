7631a8fbe89826cf5861bb1893ce0ef1ecccf6d6
Pedro <psilva@cern.ch>
2025-01-22 14:03:35 +0100

    adding explicit filter to the cfg

--- SimCalorimetry/HGCalSimProducers/test/runDIGI2RAWTest_cfg.py
+++ SimCalorimetry/HGCalSimProducers/test/runDIGI2RAWTest_cfg.py
@@ -13,8 +13,10 @@ process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 # parse command line arguments
 from FWCore.ParameterSet.VarParsing import VarParsing
 options = VarParsing('standard')
-options.register('modules', '/eos/cms/store/group/dpg_hgcal/comm_hgcal/psilva/Hackathon_2025Jan/modulelocator_layer1.txt', VarParsing.multiplicity.singleton, VarParsing.varType.string,
+options.register('modules', '/eos/cms/store/group/dpg_hgcal/comm_hgcal/psilva/Hackathon_2025Jan/modulelocator_Si.txt', VarParsing.multiplicity.singleton, VarParsing.varType.string,
                  "module locator file to use")
+options.register('filter', 'fedid==0', VarParsing.multiplicity.singleton, VarParsing.varType.string,
+                 "filter condition to apply (pandas/sql syntax)")
 options.parseArguments()
 
 # input configuration
@@ -24,11 +26,18 @@ process.source = cms.Source(
     fileNames=cms.untracked.vstring(*options.files)
 )
 
-# mapping (!dirty trick ahead! use symbolic link always with the same name)
+# mapping
 from Geometry.HGCalMapping.hgcalmapping_cff import customise_hgcalmapper
+import pandas as pd
+df = pd.read_csv(options.modules, sep='\s+')
+filt_df = df.query(options.filter)
 import os
-os.system(f'cp -v {options.modules} ${{CMSSW_BASE}}/src/modulelocator.txt')
+cmssw_base = os.environ['CMSSW_BASE']
+outmoduleloc = f'{cmssw_base}/src/modulelocator.txt'
+filt_df.to_csv(outmoduleloc, sep=' ', header=True, index=False)
 process = customise_hgcalmapper(process, modules = 'modulelocator.txt')
+del df
+print(f'Module locator @ {outmoduleloc}')
 
 process.digi2soa = cms.EDProducer('HGCalDigiSoAFiller')
 
