c04c7d3e627ca5cb0ba0171e1074082210648967
Pedro <psilva@cern.ch>
2025-05-20 12:03:47 +0200

    add roc packing

--- SimCalorimetry/HGCalSimProducers/plugins/HGCalRealisticDigisProducer.cc
+++ SimCalorimetry/HGCalSimProducers/plugins/HGCalRealisticDigisProducer.cc
@@ -9,15 +9,14 @@
 #include "FWCore/Utilities/interface/StreamID.h"
 
 #include "DataFormats/DetId/interface/DetId.h"
-#include "DataFormats/HGCDigi/interface/HGCDigiCollections.h" //This is temporary, we would have to update it to the one below.
-//#include "DataFormats/HGCalDigi/interface/HGCalDigiCollections.h"
+#include "DataFormats/HGCalDigi/interface/HGCROCChannelDataFrame.h"
 #include "DataFormats/HGCalDigi/interface/HGCalElectronicsId.h"
 #include "DataFormats/HGCalDigi/interface/HGCalDigiHost.h"
 #include "DataFormats/HGCalDigi/interface/HGCalRawDataDefinitions.h"
 
 #include "CondFormats/DataRecord/interface/HGCalElectronicsMappingRcd.h"
 #include "CondFormats/HGCalObjects/interface/HGCalMappingModuleIndexer.h"
-//#include "CondFormats/HGCalObjects/interface/HGCalMappingCellIndexer.h"
+
 #include "CondFormats/DataRecord/interface/HGCalDenseIndexInfoRcd.h"
 #include "CondFormats/DataRecord/interface/HGCalModuleConfigurationRcd.h"
 #include "CondFormats/HGCalObjects/interface/HGCalConfiguration.h"
@@ -28,7 +27,6 @@
 #include "Geometry/CaloGeometry/interface/CaloSubdetectorGeometry.h"
 #include "Geometry/HGCalGeometry/interface/HGCalGeometry.h"
 
-#include <unordered_map>
 
 class HGCalRealisticDigisProducer : public edm::stream::EDProducer<> {
   /**
@@ -43,10 +41,15 @@ private:
   void produce(edm::Event&, const edm::EventSetup&) override;
   void endStream() override {}
   void beginRun(edm::Run const&, edm::EventSetup const&) override;
-
+  
+  //ROC-related variables and methods
+  bool rocCharMode_; //characterization mode
+  HGCROCChannelDataFrame<uint32_t> rocPacker_; //helper class
+  std::vector<uint32_t> packInROCframes(hgcaldigi::HGCalDigiHost::ConstView &, size_t , size_t ); //SoA -> ROC format wrapper
+    
   //ROC digis to consume
   edm::EDGetTokenT<hgcaldigi::HGCalDigiHost> rocDigisToken_;
-  
+
   //module mapping
   edm::ESGetToken<HGCalMappingModuleIndexer, HGCalElectronicsMappingRcd> moduleIndexToken_;
   
@@ -56,6 +59,7 @@ private:
 
 //
 HGCalRealisticDigisProducer::HGCalRealisticDigisProducer(const edm::ParameterSet& iConfig) :
+  rocCharMode_( iConfig.getUntrackedParameter<bool>("ROCCharMode") ),
   rocDigisToken_( consumes<hgcaldigi::HGCalDigiHost>( iConfig.getUntrackedParameter<edm::InputTag>("ROCDigis") ) ),
   moduleIndexToken_(esConsumes()),
   fedDataToken_(produces< std::vector<uint32_t> >())
@@ -73,13 +77,12 @@ void HGCalRealisticDigisProducer::produce(edm::Event& iEvent, const edm::EventSe
   
   // retrieve logical mapping and dense indexing
   const auto& moduleIndexer = iSetup.getData(moduleIndexToken_);
-
+  
   // retrieve the ROC Digis
-  edm::Handle<hgcaldigi::HGCalDigiHost> rocDigis_;
-  iEvent.getByToken(rocDigisToken_,rocDigis_);
-
-  std::cout << rocDigis_.isValid() << std::endl;
-
+  edm::Handle<hgcaldigi::HGCalDigiHost> rocDigis;
+  iEvent.getByToken(rocDigisToken_,rocDigis);
+  auto digis_view = rocDigis->const_view();
+  
   //loop over FEDs
   auto nerxPerType = moduleIndexer.getGlobalTypesNErx();
   for(const auto &fed : moduleIndexer.getFEDReadoutSequences() ) {
@@ -88,12 +91,21 @@ void HGCalRealisticDigisProducer::produce(edm::Event& iEvent, const edm::EventSe
     size_t nmodules = fed.readoutTypes_.size();
     if(nmodules==0) continue;
 
-    std::cout << "FED id: " << fed.id << " has " << nmodules << " modules" << std::endl;
+    
     for(size_t i=0; i<nmodules; i++) {
-      
+
+      //compute the starting and ending indices
       auto readoutType = fed.readoutTypes_[i];
       auto nerx = nerxPerType[readoutType];
-      std::cout << "\t " << i << " : " << fed.chDataOffsets_[i] << " -> i+" << 37*nerx << std::endl;
+      auto idx_i = fed.chDataOffsets_[i];
+      auto idx_f = idx_i + 37*nerx;
+
+      //pack DIGIs as ROC words
+      std::vector<uint32_t> rocData = packInROCframes(digis_view, idx_i, idx_f);
+
+      //pack in ECON-data
+      std::cout << std::dec << i << " " << rocData.size() << " 0x" << std::hex << rocData[0] << std::endl;
+
     }//end loop over readout sequence
     
   } //end loop over FEDs
@@ -105,10 +117,28 @@ void HGCalRealisticDigisProducer::produce(edm::Event& iEvent, const edm::EventSe
   iEvent.emplace(fedDataToken_, std::move(fedData));
 }
 
+//
+std::vector<uint32_t> HGCalRealisticDigisProducer::packInROCframes(hgcaldigi::HGCalDigiHost::ConstView &digis_view, size_t idx_i, size_t idx_f) {
+  
+  std::vector<uint32_t> rocData(idx_f-idx_i,0);
+
+  //loop over channel digis and pack in 32b word
+  for(size_t i=idx_i; i<idx_f; i++) {
+    bool tc = digis_view.tctp()[i] & 0x1;
+    bool tp = (digis_view.tctp()[i]>>1) & 0x1;
+    rocPacker_.fill(rocCharMode_, tc, tp, digis_view.adcm1()[i], digis_view.adc()[i], digis_view.tot()[i], digis_view.toa()[i]);
+    rocData[i-idx_i] = rocPacker_.raw();
+  }
+  
+  return rocData;
+}
+
+
 // fill descriptions
 void HGCalRealisticDigisProducer::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
   edm::ParameterSetDescription desc;
   desc.addUntracked<edm::InputTag>("ROCDigis", edm::InputTag("hgcalDigis"));
+  desc.addUntracked<bool>("ROCCharMode", false);
   descriptions.addWithDefaultLabel(desc);
 }
 
